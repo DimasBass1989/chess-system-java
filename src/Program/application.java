5d224e88ed7b7d8e38c879b66b5b64f6e44198cc
Dimas Junior <dimas.junior1989@hotmail.com>
2022-08-18 12:48:11 -0300

    Starting to implement Board and Piece

--- src/Program/application.java
+++ src/Program/application.java
@@ -1,5 +1,6 @@
 package Program;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class application {
@@ -7,9 +8,6 @@ public class application {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Position pos = new Position(3, 5);
-		System.out.println(pos);
-
-	}
-
+		Board board = new Board(8,8);
+}
 }
